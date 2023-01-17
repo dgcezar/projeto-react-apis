9972d9b59c4a419f0dcd8b42fa4e92baf96bd747
dgcezar <diego.cezar13@gmail.com>
2023-01-17 12:10:48 -0300

    img readme

--- src/Pages/PokemonDetailPage/pokemondetailstyle.js
+++ src/Pages/PokemonDetailPage/pokemondetailstyle.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const PokemonDetailContainer = styled.section`
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: #5e5e5e;
 `;
 
